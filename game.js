bceade35261365af7dc05a7f430c37613b4d36aa
ErikTheCoder <50461511+ErikTheCoder@users.noreply.github.com>
2021-05-10 23:07:38 +0200

    Added multipliers
    I have now added multipliers. Everything id the GDD is now complete! I just need to expand it.

--- game.js
+++ game.js
@@ -2,6 +2,8 @@ var number = 0;
 var number_per_second = 0;
 var autocounters = 0;
 var autocounter_cost = 10;
+var multipliers = 0;
+var multiplier_cost = 10;
 var update_rate = 50;
 var lastTick = Date.now();
 
@@ -15,16 +17,24 @@ function buyAutocounter(){
         autocounters++;
     }   
 }
+function buyMultiplier(){
+    if(autocounters >= multiplier_cost){
+        autocounters -= multiplier_cost;
+        multipliers++;
+    }   
+}
 
 function ProductionLoop(diff){
-    number_per_second = autocounters;
+    number_per_second = (Math.pow(2, multipliers) * autocounters);
     autocounter_cost =  Math.pow(1.1, autocounters) * 10;
-    number += (number_per_second * diff)
+    multiplier_cost = Math.pow(1.5, multipliers) * 10;
+    number += number_per_second * diff;
 }
 function UpdateDisplay(){
     document.getElementById("numberText").innerHTML = "You have counted to " + Math.floor(number).toLocaleString();
     document.getElementById("numberPerSecondText").innerHTML = "You are counting up " + number_per_second.toLocaleString() + " per second.";
-    document.getElementById("buyAutocounterButton").innerHTML = "Buy Autocounter. Amount: " + autocounters.toLocaleString() + " Cost: " + Math.floor(autocounter_cost).toLocaleString();
+    document.getElementById("buyAutocounterButton").innerHTML = "Buy Autocounter. Amount: " + Math.floor(autocounters).toLocaleString() + " Cost: " + Math.floor(autocounter_cost).toLocaleString();
+    document.getElementById("buyMultiplierButton").innerHTML = "Buy Multiplier. Amount: " + multipliers.toLocaleString() + " Cost: " + Math.floor(multiplier_cost).toLocaleString() + " autocounters";
 }
 function gameLoop(){
     diff = (Date.now() - lastTick)/1000;
@@ -33,4 +43,5 @@ function gameLoop(){
     ProductionLoop(diff);
 }
 
+setInterval(gameLoop,update_rate);
 setInterval(gameLoop,update_rate);
