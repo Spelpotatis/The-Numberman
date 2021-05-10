e6e32b0aa12d35342287e29278797e378265ef2e
ErikTheCoder <50461511+ErikTheCoder@users.noreply.github.com>
2021-05-10 20:36:06 +0200

    Added autocounters
    I added autocounters to the game

--- game.js
+++ game.js
@@ -1,20 +1,36 @@
 var number = 0;
+var number_per_second = 0;
+var autocounters = 0;
+var autocounter_cost = 10;
 var update_rate = 50;
 var lastTick = Date.now();
-
-alert("DISCLAIMER: This is the first prototy of this, there is literally nothing except a button and a number lol");
 
 function count(){
     number++;    
 }
 
+function buyAutocounter(){
+    if(number >= autocounter_cost){
+        number -= autocounter_cost;
+        autocounters++;
+    }   
+}
+
+function ProductionLoop(diff){
+    number_per_second = autocounters;
+    autocounter_cost =  Math.pow(1.1, autocounters) * 10;
+    number += (number_per_second * diff)
+}
 function UpdateDisplay(){
-    document.getElementById("numberText").innerHTML = "You have counted to " + number.toLocaleString();
+    document.getElementById("numberText").innerHTML = "You have counted to " + Math.floor(number).toLocaleString();
+    document.getElementById("numberPerSecondText").innerHTML = "You are counting up " + number_per_second.toLocaleString() + " per second.";
+    document.getElementById("buyAutocounterButton").innerHTML = "Buy Autocounter. Amount: " + autocounters.toLocaleString() + " Cost: " + Math.floor(autocounter_cost).toLocaleString();
 }
 function gameLoop(){
-    diff = Date.now() - lastTick;
+    diff = (Date.now() - lastTick)/1000;
     lastTick = Date.now();
-    UpdateDisplay();   
+    UpdateDisplay();
+    ProductionLoop(diff);
 }
 
 setInterval(gameLoop,update_rate);
